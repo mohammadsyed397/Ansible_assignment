328796604ab86e2327fffe71737a2b6b0f318dd4
Mohammad Syed <52034679+mohammadsyed397@users.noreply.github.com>
2025-01-06 16:32:38 -0500

    Update dynamic_inventory.py

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -7,25 +7,25 @@ def main():
         "_meta": {
             "hostvars": {
                 "target-1": {
-                    "ansible_host": "3.21.113.198",
+                    "ansible_host": "ip address of instance",
                     "ansible_connection": "ssh",
                     "ansible_user": "ubuntu",
                     "ansible_ssh_private_key_file": "./munna.pem"
                 },
                 "target-2": {
-                    "ansible_host": "18.117.176.102",
+                    "ansible_host": "ip address of instance",
                     "ansible_connection": "ssh",
                     "ansible_user": "ubuntu",
                     "ansible_ssh_private_key_file": "./munna.pem"
                 },
                 "target-4": {
-                    "ansible_host": "3.142.52.177",
+                    "ansible_host": "ip address of instance",
                     "ansible_connection": "ssh",
                     "ansible_user": "ubuntu",
                     "ansible_ssh_private_key_file": "./munna.pem"
                 },
                 "target-5": {
-                    "ansible_host": "3.129.64.85",
+                    "ansible_host": "ip address of instance",
                     "ansible_connection": "ssh",
                     "ansible_user": "ubuntu",
                     "ansible_ssh_private_key_file": "./munna.pem"
